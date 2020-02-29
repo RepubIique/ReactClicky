1e9d4f59b17ac0aaf851e6aeb24612501c7eaaa9
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-02-29 15:59:39 +0800

    Added pictures and jumbotron

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container text-center">
+      <h1 className="display-4">Click on something</h1>
+      <p className="lead">You know the rules</p>
+    </div>
+  </div>
+);
+
+export default Jumbotron;
